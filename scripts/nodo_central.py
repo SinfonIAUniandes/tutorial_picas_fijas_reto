a2da65bbc0d6b911649824799cfc2856eb8b014b
dcuevasa <d.cuevas@uniandes.edu.co>
2025-09-19 22:07:22 -0500

    small fixes nodo central

--- scripts/nodo_central.py
+++ scripts/nodo_central.py
@@ -1,12 +1,14 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 
 import rospy
 from std_msgs.msg import String
 from rospy import ServiceException
+import os
 
 def main():
     rospy.init_node('nodo_central')
-    pub = rospy.Publisher('/topico_global', String, queue_size=10)
+    publicador = rospy.Publisher('/topico_global', String, queue_size=10)
+    rospy.loginfo("Publicador global creado.")
     
     # Conjunto para controlar los equipos registrados
     equipos_registrados = set()
@@ -15,15 +17,22 @@ def main():
         nombre_equipo = datos.data
         if nombre_equipo not in equipos_registrados:
             equipos_registrados.add(nombre_equipo)
-            pub.publish(String(f"{nombre_equipo}, registrado"))
+            mensaje_de_registro = String()
+            mensaje_de_registro.data = f"{nombre_equipo}, registrado"
+            publicador.publish(mensaje_de_registro)
             rospy.loginfo(f"Registrado: {nombre_equipo}")
+        else:
+            rospy.loginfo(f"Equipo {nombre_equipo} ya registrado.")
     
-    sub = rospy.Subscriber('/topico_registro', String, manejar_registro)
+    suscriptor = rospy.Subscriber('/topico_registro', String, manejar_registro)
+    rospy.loginfo("Suscriptor de registro creado.")
     
     rospy.spin()
 
 if __name__ == '__main__':
     try:
+        print("Iniciando nodo central...")
         main()
+        print("Nodo central finalizado.")
     except rospy.ROSInterruptException:
-        pass
+        os.exit(0)
